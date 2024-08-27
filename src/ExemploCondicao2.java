411f6cc0b43e0bb11098eaac6beb47bc2276f213
Luís Fernando Parizotto <164382347+LParizotto@users.noreply.github.com>
2024-08-26 21:24:38 -0300

    Aprendendo Condições JAVA

---
+++ src/ExemploCondicao2.java
@@ -0,0 +1,10 @@
+public class ExemploCondicao2 {
+
+
+    //Quero adicionar um desconto gradativo
+    //Valor Maior que 1000 - 10%
+    //Valor Maior que 2000 - 20%
+    //Valor Maior que 3000 - 30%
+    //Valor Menor igual a 1000 - 0%
+
+}
