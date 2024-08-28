52f2a5b003af65c0b07bfe62d482c8e3ee91bb9f
Luís Fernando Fappi Parizotto <164382347+LParizotto@users.noreply.github.com>
2024-08-28 19:18:20 -0300

    Aprendendo Condições JAVA

--- src/ExemploCondicao2.java
+++ src/ExemploCondicao2.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class ExemploCondicao2 {
 
 
@@ -9,21 +11,36 @@ public class ExemploCondicao2 {
 
     public static void main(String[] args) {
 
-        double valorBruto = 1500;
+        double valorBruto = 3000;
+        double desconto = 0d;
 
-        if(valorBruto > 1000) {
-            double desconto = valorBruto * 0.10;
+        // Operadores Lógicos
+        // && e
+        // || ou
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Informe o valor do bruto: ");
+        valorBruto = scanner.nextDouble();
+
+        if(valorBruto > 1000 && valorBruto <= 2000) {
+             desconto = valorBruto * 0.10;
         }
 
-        else if(valorBruto > 2000) {
-            double desconto = valorBruto * (20/100);
+        else if(valorBruto > 2000 && valorBruto <= 3000) {
+             desconto = valorBruto * (20 / 100);
         }
 
         else if(valorBruto >= 3000) {
-            double desconto = valorBruto * (30/100);
+             desconto = valorBruto * (30. / 100);
         }
 
+
+        System.out.println("Valor Bruto é:" + valorBruto);
+        System.out.println("Valor do desconto é:" + desconto);
+        System.out.println("Valor Liquido é:" + (valorBruto - desconto));
+
     }
+
 
 
 }
